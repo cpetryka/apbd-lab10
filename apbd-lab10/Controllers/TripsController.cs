5f50d3cb898beb9cf79c04d3fa9e60e3512a8190
Cezary Petryka <cezaryp10@gmail.com>
2024-05-28 14:46:15 +0200

    Add controller and register it

---
+++ apbd-lab10/Controllers/TripsController.cs
@@ -0,0 +1,22 @@
+﻿using apbd_lab10.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apbd_lab10.Controllers;
+
+[Controller]
+[Route("api/[controller]")]
+public class TripsController : ControllerBase
+{
+    private readonly ApbdContext _context;
+
+    public TripsController(ApbdContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetTrips()
+    {
+        return Ok();
+    }
+}
